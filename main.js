855d93214820be38a6d72920a097e9f6b93ef0d8
Marwan Rahim <marwan820@Marwan>
2023-05-19 18:13:00 -0500

    complete lab

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const likeButtons = document.querySelectorAll(".like-glyph");
 
-// Your JavaScript code goes here!
+  errorModal.classList.add("hidden");
 
-
-
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          button.classList.toggle("activated-heart");
+          button.classList.toggle("empty-heart");
+        })
+        .catch(() => {
+          errorModal.classList.remove("hidden");
+          const errorMessage = document.getElementById("modal-message");
+          errorMessage.textContent = "Error: Failed to like the post";
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
   });
-}
+
+  function mimicServerCall() {
+    return new Promise((resolve, reject) => {
+      const randomTimeout = Math.random() * 3000;
+      setTimeout(() => {
+        if (Math.random() < 0.5) {
+          resolve();
+        } else {
+          reject();
+        }
+      }, randomTimeout);
+    });
+  }
+});
